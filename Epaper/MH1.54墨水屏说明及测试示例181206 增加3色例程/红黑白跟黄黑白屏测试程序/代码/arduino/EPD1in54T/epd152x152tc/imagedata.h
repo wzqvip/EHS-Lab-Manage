e1a319284bf2e42004a6c1a59d64fcdf472235c5
Taco <wangziqi2002vip@163.com>
2022-10-18 20:59:00 +0800

    files

--- Epaper/MH1.54墨水屏说明及测试示例181206 增加3色例程/红黑白跟黄黑白屏测试程序/代码/arduino/EPD1in54T/epd152x152tc/imagedata.h
+++
@@ -1,7 +0,0 @@
-#ifndef _IMAGE_H_
-#define _IMAGE_H_
-
-extern const unsigned char IMAGE_BLACK[];
-extern const unsigned char IMAGE_YELLOW[];
-
-#endif
--- Epaper/MH1.54墨水屏说明及测试示例181206 增加3色例程/红黑白跟黄黑白屏测试程序/代码/arduino/EPD1in54T/src/EPD_Display.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EPD_Display_h__
-#define __EPD_Display_h__
-
-#include <./epdlib/UC8151c_152x152.h>
-#include <./epdlib/UC8154c_200X200.h>
-
-#endif
